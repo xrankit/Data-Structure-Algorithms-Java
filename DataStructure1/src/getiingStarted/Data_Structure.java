b095259b48e561903d54a32a79d5a3452fde5f66
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2021-10-02 20:29:31 +0530

    Day1

---
+++ DataStructure1/src/getiingStarted/Data_Structure.java
@@ -0,0 +1,11 @@
+package getiingStarted;
+
+public class Data_Structure {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to the data Structre");
+		System.out.print("Welcome to thet data structure & Algorithms");
+	}
+
+}
