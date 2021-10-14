3931ba4016ce87a2d171a17176191e8548fc3495
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2021-10-14 20:32:11 +0530

    pattern4

--- DataStructure1/src/pattern/Pattern4.java
+++ DataStructure1/src/pattern/Pattern4.java
@@ -1,10 +1,29 @@
 package pattern;
 
+import java.util.Scanner;
 public class Pattern4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int gap = 0; 
+		int star = n;
+		for(int i =0;i<n;i++)
+		{
+			for(int j =0;j<gap;j++)
+			{
+				System.out.print("\t");
+			}
+			for(int j =star;j>0;j--)
+			{
+				System.out.print("*\t");
+			}
+			gap++;
+			star--;
+			
+			System.out.println();
+		}
 	}
 
 }
