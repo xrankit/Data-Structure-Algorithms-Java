e2eb6c01a2ff1fe7acc19632f10475796030285d
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2021-12-07 02:47:58 +0530

    commit

---
+++ DataStructure1/src/pattern/pattern14.java
@@ -0,0 +1,21 @@
+package pattern;
+import java.util.Scanner;
+public class pattern14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for(int i =0;i<n; i++)
+		{
+			for ( int j = 0;j<=i;j++)
+			{
+				System.out.print((char)('A'+ j));
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
