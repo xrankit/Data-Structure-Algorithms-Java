7001e3bd8e3b2a760d442569270bc5cf11eeb4a8
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2021-12-09 00:03:45 +0530

    commit

---
+++ DataStructure1/src/pattern/pattern20.java
@@ -0,0 +1,16 @@
+package pattern;
+
+public class pattern20 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		1	
+//		1	1	
+//		1	2	1	
+//		1	3	3	1	
+//		1	4	6	4	1
+		
+		
+	}
+
+}
