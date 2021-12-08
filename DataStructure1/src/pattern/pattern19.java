e6c60fdd353015582d702ebc6ea6a3366981265d
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2021-12-08 23:35:27 +0530

    commit

--- DataStructure1/src/pattern/pattern19.java
+++ DataStructure1/src/pattern/pattern19.java
@@ -13,7 +13,23 @@ public class pattern19 {
 //		55	89	144	233	377
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		
+		int a = 0;
+		int b = 1;
+		int c = a + b;
+		System.out.println(a);
+		System.out.println(b+" "+c);
+		for(int i = 3; i<=n;i++)
+		{
+			for(int j = 1; j<=i;j++)
+			{
+				a=b;
+				b=c;
+				c=a+b;
+				System.out.print(c+" ");
+				
+				
+			}
+			System.out.println();
+		}
 	}
-
 }
