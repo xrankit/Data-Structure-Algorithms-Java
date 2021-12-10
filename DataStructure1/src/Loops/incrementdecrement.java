9b5aa2eaadf6b7aa87074a6ae196f590f3c55293
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2021-12-11 01:02:31 +0530

    commit

---
+++ DataStructure1/src/Loops/incrementdecrement.java
@@ -0,0 +1,23 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class incrementdecrement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		if(a-- >0 | ++b>2)
+		{
+			System.out.println("ankit Yadav");
+		}
+		else 
+		{
+			System.out.println("yadav Ankit");
+		}
+	}
+
+}
