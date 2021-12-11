2721e3d998840b5c770d4cbdf2d5e2e43a8a74ca
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2021-12-12 00:31:26 +0530

    commit

---
+++ DataStructure1/src/Loops/binarytodecimal.java
@@ -0,0 +1,13 @@
+package Loops;
+import java.util.Scanner;
+import java.lang.Math;
+public class binarytodecimal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+	}
+
+}
