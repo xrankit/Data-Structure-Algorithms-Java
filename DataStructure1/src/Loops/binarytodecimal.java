e2443237455294267e8609396444724832ac85c0
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2021-12-12 16:50:20 +0530

    commit

--- DataStructure1/src/Loops/binarytodecimal.java
+++ DataStructure1/src/Loops/binarytodecimal.java
@@ -7,7 +7,19 @@ public class binarytodecimal {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		
+		int i = 0;
+		int ans=0;
+		while(n!=0)
+		{
+			int bit = n%10;
+			if(bit ==1)
+			{
+				ans = ans + (int)(Math.pow(2, i));
+			}
+			n=n/10;
+			i++;
+		}
+		System.out.println(ans);
 	}
 
 }
