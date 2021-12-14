7444e1c721c655461b869c7ed92ace38a222de6a
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2021-12-14 23:47:47 +0530

    commit

---
+++ DataStructure1/src/pattern/pattern28.java
@@ -0,0 +1,68 @@
+package pattern;
+
+public class pattern28 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		A B C D E F 
+//
+//		A B C D E 
+//
+//		A B C D 
+//
+//		A B C 
+//
+//		A B 
+//
+//		A 
+//
+//		A 
+//
+//		A B 
+//
+//		A B C 
+//
+//		A B C D 
+//
+//		A B C D E 
+//
+//		A B C D E F
+		
+		for (int i = 5; i >= 0; i–)
+
+		{
+
+		   int alphabet = 65;
+
+		   for (int j = 0; j <= i; j++)
+
+		   {
+
+		       System.out.print((char) (alphabet + j) + ” “);
+
+		   }
+
+		   System.out.println();
+
+		}
+
+		for (int i = 0; i<= 5; i++)
+
+		{
+
+		   int alphabet = 65;
+
+		   for (int j = 0; j <= i; j++)
+
+		   {
+
+		       System.out.print((char) (alphabet + j) + ” “);
+
+		   }
+
+		   System.out.println();
+
+		}
+	}
+
+}
