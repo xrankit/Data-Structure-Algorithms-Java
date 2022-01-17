7783dd71e13159ac8a0b4e73be57928a15caee61
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2022-01-18 02:45:41 +0530

    commit

---
+++ Arrays/src/basicarray/ArrayUse.java
@@ -0,0 +1,25 @@
+package basicarray;
+
+public class ArrayUse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//initially all elements in integer array is zero i.e, initialize with zero;
+		int [] arrInt = new int[5];
+		System.out.println(arrInt[0]);
+		
+		//initially all elements in character array is initialize with null values;
+		char [] charArr = new char[5];
+		System.out.println(charArr[0]);
+		
+		// initially all elements in double array is initialize with zero.zero values;
+		double [] doubleArr = new double[5];
+		System.out.println(doubleArr[0]);
+		
+		//initially all elements in boolean array is initialize all elements with boolean value "false";
+		boolean arr[] = new boolean[5];
+	    System.out.println(arr[0]);
+	}
+
+}
