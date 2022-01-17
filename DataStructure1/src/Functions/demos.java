4a756bc821a5c7a1362f6e326f9597ee046a2b6f
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2022-01-17 12:52:21 +0530

    initial commit

---
+++ DataStructure1/src/Functions/demos.java
@@ -0,0 +1,17 @@
+package Functions;
+
+public class demos {
+	
+	 public static void demo(int a, int b)
+	 {
+		System.out.println(a + " " + b);
+	 } 
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5;
+		int b = 15; 
+		demo(a,b);
+	}
+
+}
