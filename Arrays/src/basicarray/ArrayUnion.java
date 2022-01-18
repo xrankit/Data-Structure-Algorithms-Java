77ca9f87d6c1b39f44b84dc3a7ea5d0bc083798a
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2022-01-19 00:21:08 +0530

    update

--- Arrays/src/basicarray/ArrayUnion.java
+++ Arrays/src/basicarray/ArrayUnion.java
@@ -1,11 +1,12 @@
 package basicarray;
 
 import java.util.Scanner;
-
+import java.util.Arrays;
 public class ArrayUnion {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
 		
 		int n = sc.nextInt();
@@ -14,41 +15,51 @@ public class ArrayUnion {
 		{
 			arr1[i] = sc.nextInt();
 		}
-		
+		Arrays.sort(arr1);
 		int m = sc.nextInt();
 		int arr2[] = new int[m];
 		for(int i =0;i<m;i++)
 		{
 			arr2[i] = sc.nextInt();
 		}
+		Arrays.sort(arr2);
+		
 		//two pointer algorithm
 		
 		int i =0,j=0;
 		while(i<n && j<m)
 		{
-			if(arr1[i]!=arr2[j])
+			if(arr1[i]<arr2[j])
 			{
-				System.out.println(arr1[i]);
-				i++;
+			    System.out.println(arr1[i]);
+			    i++;
+			}
+			
+			else if(arr1[i]>arr2[j])
+			{
 				System.out.println(arr2[j]);
 				j++;
 			}
 			else
 			{
-				System.out.println(arr1[i]);
-				i++;
+				System.out.println(arr2[j]);
 				j++;
+				i++;
 			}
 			
-//			else if(arr1[i]>arr2[j])
-//			{
-//				i++;
-//			}
-//			else
-//			{
-//				++;
-//			}
 		}
+		while (i < n)
+		{
+			System.out.println(arr1[i]);
+			i++;
+		}
+            
+		
+        while (j < m)
+        {
+            System.out.println(arr2[j]);
+            j++;
+        }
 	}
 
 }
