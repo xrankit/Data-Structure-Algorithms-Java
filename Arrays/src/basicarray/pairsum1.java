9fe4d678cc8a5e105cf6027af3311c213ff9206d
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2022-01-19 00:29:47 +0530

    commit

---
+++ Arrays/src/basicarray/pairsum1.java
@@ -0,0 +1,25 @@
+package basicarray;
+
+import java.util.Scanner;
+import java.util.Arrays;
+public class pairsum1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+		for(int i =0; i<n;i++)
+		{
+			arr[i] = sc.nextInt();
+		}
+		Arrays.sort(arr);
+		
+		int temp = sc.nextInt();
+		int i =0;
+		int r = n-1;
+		
+	}
+
+}
