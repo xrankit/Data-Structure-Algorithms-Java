e31e9914b905bbf2e7f7969c6b12367428b68b16
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2022-01-20 02:37:31 +0530

    update

--- Arrays/src/basicarray/PushZerostoend.java
+++ Arrays/src/basicarray/PushZerostoend.java
@@ -1,10 +1,40 @@
 package basicarray;
 
+import java.util.Scanner;
+
 public class PushZerostoend {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			int arr[] = new int[n];
+			for(int i =0;i<n;i++)
+			{
+				arr[i] = sc.nextInt();
+			}
+			int count =0;
+			for(int i=0;i<n;i++)
+			{
+				if(arr[i]!=0)
+				{
+					arr[count]=arr[i];
+					count++;
+				}
+			}
+			while(count<n)
+			{
+				arr[count] = 0;
+				count++;
+			}
+			for(int i =0;i<n;i++)
+			{
+				System.out.print(arr[i] + "\t");
+			}
+			
+		}
 	}
-
 }
