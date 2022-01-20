8faf402c6abd519a0ec748980eddc2df4de8d8c4
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2022-01-21 02:42:29 +0530

    commit

---
+++ Arrays/src/basicarray/Rotatearray.java
@@ -0,0 +1,25 @@
+
+package basicarray;
+
+import java.util.Scanner;
+
+public class Rotatearray {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t>0) 
+		{
+			int n = sc.nextInt();
+			int arr[] = new int[n];
+			for(int i =0;i<n;i++)
+			{
+				arr[i] = sc.nextInt();
+			}
+
+		}
+	}
+
+}
