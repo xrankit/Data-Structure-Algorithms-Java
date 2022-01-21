fd377e4f0b6c09e04fd24e1651cfb430db3976ae
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2022-01-21 23:26:45 +0530

    commit

--- Arrays/src/basicarray/Rotatearray.java
+++ Arrays/src/basicarray/Rotatearray.java
@@ -4,7 +4,17 @@ package basicarray;
 import java.util.Scanner;
 
 public class Rotatearray {
-
+	
+	public static void rotateArray(int arr[], int n )
+	{
+		int temp = arr[0];
+		for(int i =0;i<n-1; i++)
+		{
+			arr[i] = arr[i+1];
+		}
+		arr[n-1] = temp;
+			
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -18,7 +28,17 @@ public class Rotatearray {
 			{
 				arr[i] = sc.nextInt();
 			}
-
+			int d = sc.nextInt();
+			
+			for(int i =0; i<d;i++)
+			{
+				rotateArray(arr, n);
+			}
+			
+			for (int i = 0; i < n; i++)
+			{
+	            System.out.print(arr[i] + " ");
+			}
 		}
 	}
 
