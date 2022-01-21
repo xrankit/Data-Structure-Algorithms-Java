0aff5372100fde74b8902dd49604e075dafe5ff9
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2022-01-21 23:28:20 +0530

    commit

---
+++ Arrays/src/basicarray/Sort012.java
@@ -0,0 +1,24 @@
+package basicarray;
+
+import java.util.Scanner;
+
+public class Sort012 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t > 0) 
+		{
+			int n = sc.nextInt();
+			int arr[] = new int[n];
+			for(int i =0;i<n;i++)
+			{
+				arr[i] = sc.nextInt();
+			}
+			int count =0;
+			
+		}
+	}
+
+}
