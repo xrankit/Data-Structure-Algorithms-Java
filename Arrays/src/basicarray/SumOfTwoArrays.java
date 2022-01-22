d53a69bc8f8c0d1678162574dfec464f3a5a8d4e
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2022-01-22 23:33:16 +0530

    commit

---
+++ Arrays/src/basicarray/SumOfTwoArrays.java
@@ -0,0 +1,37 @@
+package basicarray;
+
+import java.util.Scanner;
+
+public class SumOfTwoArrays {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t > 0) 
+		{
+			int n = sc.nextInt();
+			int arr1[] = new int[n];
+			for(int i =0;i<n;i++)
+			{
+				arr1[i] = sc.nextInt();
+			}
+			
+			int m = sc.nextInt();
+			int arr2[] = new int[m];
+			for(int i =0;i<n;i++)
+			{
+				arr2[i] = sc.nextInt();
+			}
+			int sum =0;
+			int i =n-1;
+			int j = m-1;
+			while(i < 0 && j < 0)
+			{
+				
+			}
+		}
+	}
+
+}
