f3385689da0f766e99aaa156c1d5175eb453841a
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2022-01-22 23:33:43 +0530

    update

--- Arrays/src/basicarray/Sort012.java
+++ Arrays/src/basicarray/Sort012.java
@@ -16,8 +16,36 @@ public class Sort012 {
 			{
 				arr[i] = sc.nextInt();
 			}
-			int count =0;
-			
+			int count0 =0;
+			int count1 = 0;
+			int count2 =0;
+			for(int i=0;i<n;i++)
+			{
+				if(arr[i]==0)
+				{
+					count0++;
+				}
+				else if(arr[i]==1)
+				{
+					count1++;
+				}
+				else
+				{
+					count2++;
+				}
+			}
+			for(int i =0;i<count0;i++)
+			{
+				System.out.print("0"+" ");
+			}
+			for(int i =0;i<count1;i++)
+			{
+				System.out.print("1"+" ");
+			}
+			for(int i =0;i<count2;i++)
+			{
+				System.out.print("2"+" ");
+			}
 		}
 	}
 
