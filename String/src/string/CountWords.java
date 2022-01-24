d47092425bf616709df01d63e9ba5a8b9754c2da
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2022-01-24 23:40:56 +0530

    update

--- String/src/string/CountWords.java
+++ String/src/string/CountWords.java
@@ -4,7 +4,20 @@ public class CountWords {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+//		Problem Description: #### Given a string S, 
+//		count number of words in the string.
+//		Note : Assume there is single space between two words and 
+//		there are no extra spaces before and after words.
+		
+		String str = "count number of words in the string";
+		int totalCount = str.length();
+		System.out.println("String with gap " +totalCount);
+		int replace_gap = str.replace(" ","").length();
+		System.out.println("total number of letters "+ replace_gap);
+		int count = totalCount - replace_gap;
+		System.out.println("number of gap "+count);
+		
+		
 	}
 
 }
