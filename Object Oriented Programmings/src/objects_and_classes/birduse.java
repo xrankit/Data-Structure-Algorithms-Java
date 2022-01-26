67803452709924b8c332883db9915331f4747fa0
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2022-01-26 21:36:41 +0530

    commit

---
+++ Object Oriented Programmings/src/objects_and_classes/birduse.java
@@ -0,0 +1,34 @@
+package objects_and_classes;
+
+public class birduse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		bird b1 = new bird();
+		b1.Name = "Sparrow";
+		b1.size = 1;
+		b1.wing = "small";
+		b1.beak = "short";
+		
+		bird b2 = new bird();
+		b1.Name = "parrot";
+		b1.size = 2;
+		b1.wing = "medium";
+		b1.beak = "small";
+		
+		bird b3 = new bird();
+		b1.Name = "Wood Peaker";
+		b1.size = 2;
+		b1.wing = "medium";
+		b1.beak = "long";
+		
+		bird b4 = new bird();
+		b1.Name = "peacock";
+		b1.size = 5;
+		b1.wing = "long";
+		b1.beak = "medium";
+		
+		
+	}
+
+}
