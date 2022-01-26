daace11976e7dd1d2961b64183e8954abffa237b
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2022-01-26 21:37:05 +0530

    commit

---
+++ Object Oriented Programmings/src/objects_and_classes/StudentUse.java
@@ -0,0 +1,24 @@
+package objects_and_classes;
+
+public class StudentUse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s1 = new Student();
+		s1.name ="Ankit";
+		s1.rollNo = 120016;
+		s1.age = 20;
+		System.out.println(s1.name);
+		System.out.println(s1.rollNo);
+		System.out.println(s1.age);
+		
+		Student s2 = new Student();
+		s2.name ="Ayush";
+		s2.rollNo = 120029;
+		s2.age = 20;
+		System.out.println(s2.name);
+		System.out.println(s2.rollNo);
+		System.out.println(s2.age);
+	}
+
+}
