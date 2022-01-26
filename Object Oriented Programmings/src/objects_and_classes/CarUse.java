ef8819ef86dec0535d376168920f35f98e4fe4fc
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2022-01-26 21:36:51 +0530

    commit

---
+++ Object Oriented Programmings/src/objects_and_classes/CarUse.java
@@ -0,0 +1,19 @@
+package objects_and_classes;
+	
+class Car{
+    String color;
+    double price;
+}
+
+public class CarUse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Car c = new Car();
+		 c.color= "white";
+		 System.out.println(c.color);
+         //Figure out the correct statement 
+         //	to set color of the car object referred by c to “white”
+	}
+
+}
