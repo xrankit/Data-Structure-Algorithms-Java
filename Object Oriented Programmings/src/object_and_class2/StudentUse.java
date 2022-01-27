531999e9a606c1c850356164bc0b250c52f6051b
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2022-01-27 17:38:34 +0530

    commit

---
+++ Object Oriented Programmings/src/object_and_class2/StudentUse.java
@@ -0,0 +1,25 @@
+package object_and_class2;
+
+class Student{
+    String name;
+    int rollNo;
+    
+    Student(int num)
+    {
+	rollNo = num;
+    }	
+    public void print(){
+//	System.out.print(name +" " + rollNo +” “);
+    }
+}
+
+public class StudentUse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		 Student s = new Student();
+//         s.rollNo = 15;
+//         s.print();
+	}
+
+}
