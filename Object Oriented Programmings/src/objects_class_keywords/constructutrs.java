c9a75e8d3e7535afedf6a4de3efb49942a648bc7
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2022-01-28 23:44:21 +0530

    commit

---
+++ Object Oriented Programmings/src/objects_class_keywords/constructutrs.java
@@ -0,0 +1,22 @@
+package objects_class_keywords;
+
+public class constructutrs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		
+////		Constructors and methods differ in three aspects of the signature: modifiers, return type, and name. Like methods, c
+//		onstructors can have any of the access modifiers: public, protected, private, or none (often called package or friendly).
+//		Unlike methods, constructors can take only access modifiers. Therefore, constructors cannot be abstract, final, native, static,
+//
+////
+////		The return types are very different too. Methods can have any valid return type, or no return type, in which case the return 
+//		type is given as void. Constructors have no return type, not even void.
+////
+////			Finally, in terms of the signature, methods and constructors have different names. Constructors have the same name as
+//				their class; by convention, methods use names other than the class name. If the Java program follows normal conventions, 
+//				methods will start with a lowercase letter, constructors with an uppercase letter. Also, constructor names are usually
+//				nouns because class names are usually nouns; method names usually indicate actions.
+		}
+
+}
