93b2faa5ecd254b4b2c38e799e58106108e79a04
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2022-01-28 23:44:04 +0530

    commit

---
+++ Object Oriented Programmings/src/object_class/FractionUse.java
@@ -0,0 +1,11 @@
+package object_class;
+
+public class FractionUse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Fraction f1 = new Fraction(20,30);
+		f1.print();
+	}
+
+}
