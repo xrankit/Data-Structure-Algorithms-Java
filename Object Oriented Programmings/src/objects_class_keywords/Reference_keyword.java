b81690a94f82a588a7f27b011aac16eb927102f1
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2022-01-28 23:45:20 +0530

    commit

---
+++ Object Oriented Programmings/src/objects_class_keywords/Reference_keyword.java
@@ -0,0 +1,12 @@
+package objects_class_keywords;
+
+public class Reference_keyword {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		Problem Name: Reference keyword
+//		Problem Description: ####Which keyword works as a reference to the current Object whose Method or constructor is being invoked ?
+//		Options: this keyword
+	}
+
+}
