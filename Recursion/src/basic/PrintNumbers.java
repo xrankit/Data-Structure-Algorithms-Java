426b61cab63d30db6c662096109648627223facf
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2022-01-29 23:39:31 +0530

    commit

---
+++ Recursion/src/basic/PrintNumbers.java
@@ -0,0 +1,19 @@
+package basic;
+
+public class PrintNumbers 
+{
+	public static void number(int n)
+	{
+		if(n==1)
+		{
+			return 1;
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
