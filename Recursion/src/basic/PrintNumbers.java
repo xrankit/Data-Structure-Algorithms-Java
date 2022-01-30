4610fe17751426170a6876a93a6ce18f74a3a3f6
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2022-01-30 23:23:32 +0530

    update

--- Recursion/src/basic/PrintNumbers.java
+++ Recursion/src/basic/PrintNumbers.java
@@ -1,19 +1,22 @@
 package basic;
 
+import java.util.Scanner;
+
 public class PrintNumbers 
 {
 	public static void number(int n)
 	{
-		if(n==1)
+		if(n>0)
 		{
-			return 1;
+			number(n-1);
+			System.out.print(n+" ");
 		}
-		
 	}
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		number(n);
 	}
 
 }
