7c2b89d901d6645658c08af2c57d6e08b1204023
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2022-02-05 03:13:00 +0530

    commit

---
+++ Recursion/src/Recursion1b/ReplacePi.java
@@ -0,0 +1,29 @@
+package Recursion1b;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ReplacePi {
+	
+	public static int replacePi(String str)
+	{
+		
+		return 0;
+		
+	}
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+		//Problem Name: Replace pi (recursive)
+		//Problem Level: MEDIUM
+		//Problem Description: ####Given a string, compute recursively a new string where all
+		//appearances of "pi" have been replaced by "3.14".
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		
+	}
+
+}
