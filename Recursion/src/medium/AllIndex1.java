2be44e011084cf3bcafd26b69148c9cba7785b29
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2022-02-04 23:12:20 +0530

    commit

---
+++ Recursion/src/medium/AllIndex1.java
@@ -0,0 +1,21 @@
+package medium;
+import java.util.*;
+import java.io.*;
+public class AllIndex1 {
+
+	public static void main(String[] args) throws Exception
+	{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int arr[] = new int[n];
+		for(int i = 0; i<n;i++)
+		{
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		int x = Integer.parseInt(br.readLine());
+//		all done in AllIndex but in more time complexity
+	}
+
+}
