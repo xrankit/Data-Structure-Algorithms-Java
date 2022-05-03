c6dccd192438ab0256aa442230a45b9fb7b49fb2
Ankit Yadav <66520710+ankiii07@users.noreply.github.com>
2022-05-03 16:30:04 +0530

    Day-3

---
+++ LinkedList/src/singleLinkedList/Nodes.java
@@ -0,0 +1,12 @@
+package singleLinkedList;
+
+import singleLinkedList.Question.Node;
+
+public class Nodes<T> {
+		  T data;
+		  Nodes<T> next;
+		  Nodes(T data){
+			this.data = data;
+		  }
+	    }
+
